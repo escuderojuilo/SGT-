26cd9d281293b96afe098e5f9a1fe8fc2d45fc1c
escuderojuilo <72509757+escuderojuilo@users.noreply.github.com>
2025-05-16 12:43:05 -0600

    Add files via upload

---
+++ InfoContacto.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Información de Contacto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="InfoContacto.css">
+</head>
+<body>
+
+<div class="container-fluid g-0 mb-3">
+    <div class="row">
+        <div class="col-12 p-0">
+            <img src="IMG/encabezado.jpg" alt="SOPORTEC - Sistema de Soporte Técnico" class="w-100">
+        </div>
+    </div>
+</div>        
+
+<div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
+                <div class="card shadow">
+                    <div class="card-header custom-card-header text-white">
+                        <h4 class="mb-0">Informacion de Contacto</h4>
+                    </div>
+                    <div class="card-body">
+                        <div id="piechart" style="width: 100px; "></div>
+                        <ul class="list-unstyled">
+                            <li><strong>Nombre:</strong> Juan Pérez</li>
+                            <li><strong>Correo electrónico:</strong> juan.perez@ejemplo.com</li>
+                            <li><strong>Teléfono:</strong> +52 123 456 7890</li>
+                            <li><strong>Dirección:</strong> Calle Falsa 123, Ciudad, País</li>
+                            <li><strong>Horario:</strong> Lunes a Viernes, 9:00 - 18:00</li>
+                        </ul>
+                            <div class="d-grid">
+                                <form action="index.php" method="post">
+                                    <button type="submit" class="btn custom-btn-continuar btn-lg">Regresar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
