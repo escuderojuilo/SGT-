44892e87f55b1116a4c4d5c9582a62ca7fba6244
escuderojuilo <72509757+escuderojuilo@users.noreply.github.com>
2025-06-04 13:16:45 -0600

    Mensaje de sesión ya activa
    Mensaje que le indica al usuario que su sesión ya esta activa

---
+++ SesionActiva.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Información de Contacto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="InfoContacto.css">
+</head>
+<body>
+
+<div class="container-fluid g-0 mb-3">
+    <div class="row">
+        <div class="col-12 p-0">
+            <img src="IMG/encabezadoHD.jpg" alt="SOPORTEC - Sistema de Soporte Técnico" class="w-100">
+        </div>
+    </div>
+</div>        
+
+<div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div id="piechart" style="width: 100px; "></div>
+                        <ul class="list-unstyled">
+                            <li>La sesión ya se encuentra activa.</li>
+                            <li>Por favor, cierre la sesión actual antes de iniciar una nueva.</li>
+                        </ul>
+                            <div class="d-grid">
+                                <form action="index.php" method="post">
+                                    <button type="submit" class="btn custom-btn-continuar btn-lg">Regresar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
