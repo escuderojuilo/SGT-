817916d47e97e012c31cb2faa550bcb04a9fceb2
escuderojuilo <72509757+escuderojuilo@users.noreply.github.com>
2025-06-19 10:46:50 -0600

    Update AltaSoftware.php

--- AltaSoftware.php
+++ AltaSoftware.php
@@ -20,7 +20,8 @@
             </div>
         </div>
 
-    <nav class="navbar navbar-expand-lg custom-navbar mb-4">
+    <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg custom-navbar mb-4">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
@@ -35,6 +36,7 @@
                             <li><a class="dropdown-item" href="/SGT-Boostrap/AltaInventario.php">Alta Inventario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/altaUsuario.php">Alta Usuario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/AltaSoportes.php">Alta Soportes</a></li>
+                            <li><a class="dropdown-item" href="/SGT-Boostrap/AltaSoftware.php">Alta Software</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
@@ -44,6 +46,7 @@
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/SGT-Boostrap/ConsultaUsuarios.php">Usuario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/Consultas.php">Equipos</a></li>
+                            <li><a class="dropdown-item" href="/SGT-Boostrap/ConsultaSoftware.php">Software</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
