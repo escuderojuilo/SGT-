3263fba6a827136e633ceeb3e166b2584fbfb21a
escuderojuilo <72509757+escuderojuilo@users.noreply.github.com>
2025-06-19 10:55:29 -0600

    Update EstadisticasSS.php

--- EstadisticasSS.php
+++ EstadisticasSS.php
@@ -34,6 +34,7 @@ foreach ($resul as $row) {
             </div>
         </div>
 
+        <!-- Navigation -->
         <nav class="navbar navbar-expand-lg custom-navbar mb-4">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -49,6 +50,7 @@ foreach ($resul as $row) {
                             <li><a class="dropdown-item" href="/SGT-Boostrap/AltaInventario.php">Alta Inventario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/altaUsuario.php">Alta Usuario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/AltaSoportes.php">Alta Soportes</a></li>
+                            <li><a class="dropdown-item" href="/SGT-Boostrap/AltaSoftware.php">Alta Software</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
@@ -58,6 +60,7 @@ foreach ($resul as $row) {
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/SGT-Boostrap/ConsultaUsuarios.php">Usuario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/Consultas.php">Equipos</a></li>
+                            <li><a class="dropdown-item" href="/SGT-Boostrap/ConsultaSoftware.php">Software</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
@@ -91,7 +94,7 @@ foreach ($resul as $row) {
             </div>
         </nav>
     </div>
-
+         
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-lg-9">
