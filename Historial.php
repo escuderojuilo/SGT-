b3f40d39578993dae11f6005d5de10f371786b2b
escuderojuilo <72509757+escuderojuilo@users.noreply.github.com>
2025-06-19 10:51:02 -0600

    Update Historial.php

--- Historial.php
+++ Historial.php
@@ -37,6 +37,7 @@
                             <li><a class="dropdown-item" href="/SGT-Boostrap/AltaInventario.php">Alta Inventario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/altaUsuario.php">Alta Usuario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/AltaSoportes.php">Alta Soportes</a></li>
+                            <li><a class="dropdown-item" href="/SGT-Boostrap/AltaSoftware.php">Alta Software</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
@@ -46,6 +47,7 @@
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/SGT-Boostrap/ConsultaUsuarios.php">Usuario</a></li>
                             <li><a class="dropdown-item" href="/SGT-Boostrap/Consultas.php">Equipos</a></li>
+                            <li><a class="dropdown-item" href="/SGT-Boostrap/ConsultaSoftware.php">Software</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
